a9272dffb58d8f9e482f3e6c7d5330ee058f7cbe
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-04-10 15:16:47 -0700

    Regen files

---
+++ libs/zzz/stats/src/mappedStats/char/maps/Soldier11.ts
@@ -0,0 +1,28 @@
+import type { CharacterKey } from '@genshin-optimizer/zzz/consts'
+import { getCharStat } from '../../../char'
+
+const key: CharacterKey = 'Soldier11'
+const data_gen = getCharStat(key)
+
+let b = 0, d = 0, s = 0, c = 0 , a = 0
+
+// TODO: Load scalings
+const dm = {
+  basic: {
+    attackA: data_gen.skillParams.basic[b++],
+  },
+  dodge: {
+    attackA: data_gen.skillParams.dodge[d++],
+  },
+  special: {
+    attackA: data_gen.skillParams.special[s++],
+  },
+  chain: {
+    attackA: data_gen.skillParams.chain[c++],
+  },
+  assist: {
+    attackA: data_gen.skillParams.assist[a++],
+  },
+} as const
+
+export default dm
